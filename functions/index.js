4e54209b1940575d5ca8244d7678ef03efe7ad6e
Jags <jagannath.vaikuntham@originenergy.com.au>
2018-07-05 15:47:50 +1000

    params

--- functions/index.js
+++ functions/index.js
@@ -4,22 +4,26 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
    
 console.log("REQUEST :: ", JSON.stringify(request.body));
 
-switch (request.queryResult.queryText){
-    case '100kwh':
-            response.setHeader('Content-Type', 'application/json');
-            response.send(JSON.stringify({"fulfillmentText": "Response for 100kwh" }));
-            break;
+// switch (request.queryResult.queryText){
+//     case '100kwh':
+//             response.setHeader('Content-Type', 'application/json');
+//             response.send(JSON.stringify({"fulfillmentText": "Response for 100kwh" }));
+//             break;
 
-    case '600kwh':
-            response.setHeader('Content-Type', 'application/json');
-            response.send(JSON.stringify({"fulfillmentText": "Response for 600kwh" }));
-            break;
+//     case '600kwh':
+//             response.setHeader('Content-Type', 'application/json');
+//             response.send(JSON.stringify({"fulfillmentText": "Response for 600kwh" }));
+//             break;
 
-     default:
-            response.setHeader('Content-Type', 'application/json');
-            response.send(JSON.stringify({"fulfillmentText": "Default Response" }));
-}
-    
-//response.setHeader('Content-Type', 'application/json');
-//response.send(JSON.stringify({"fulfillmentText": "Hai Chai Hooli" }));
+//      default:
+//             response.setHeader('Content-Type', 'application/json');
+//             response.send(JSON.stringify({"fulfillmentText": "Default Response" }));
+// }
+
+var energyConsumption = request.queryResult.parameters.annual-energy-consumption;
+var avgCost = energyConsumption / 4 ;
+var responseText = "Avg Cost for appliance per year is : " + avgCost;
+
+response.setHeader('Content-Type', 'application/json');
+response.send(JSON.stringify({"fulfillmentText":  responseText}));
  });
