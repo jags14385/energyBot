9b7de1046b75d669394c0bbd0bca5b9d4f538022
chai hooli <chai.hooli@originenergy.com.au>
2018-07-05 15:41:54 +1000

    format switch1

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,12 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 console.log("REQUEST :: ", JSON.stringify(request.body));
 
 switch (request.queryResult.queryText){
-    case '100kwh':
+    case "100kwh":
             response.setHeader('Content-Type', 'application/json');
             response.send(JSON.stringify({"fulfillmentText": "Response for 100kwh" }));
             break;
 
-    case '600kwh':
+    case "600kwh":
             response.setHeader('Content-Type', 'application/json');
             response.send(JSON.stringify({"fulfillmentText": "Response for 600kwh" }));
             break;
