e49354864552991d76aa3b1ee51254409ea46d2b
Jags <jagannath.vaikuntham@originenergy.com.au>
2018-07-05 18:12:45 +1000

    ROC

--- functions/index.js
+++ functions/index.js
@@ -8,16 +8,27 @@ var db = admin.firestore();
 exports.helloWorld = functions.https.onRequest((request, response) => {
    
 console.log("REQUEST :: ", JSON.stringify(request.body));
+var responseText , avgCost;
 
 var energyConsumption = request.body.queryResult.parameters['annual-energy-consumption'];
-var avgCost = energyConsumption / 4 ;
-var responseText = "Avg Cost for appliance per year is : " + avgCost;
+
+if (energyConsumption !== null ) {
+    avgCost = energyConsumption / 4 ;
+    responseText = "Avg Cost for appliance per year is : " + avgCost;
+}
+
+var username = request.body.queryResult.parameters['username'];
+var device = request.body.queryResult.parameters['device'];
+
+if ( username !== null && device !== null) {
+    console.log("ROC:: IINFIOIIIIII ", username , device );
+}
 
 db.collection('Refrigator').get()
     .then((snapshot) => {
       snapshot.forEach((doc) => {
-        console.log("DOC::: :",doc.id, '=>', doc.data());
-      });
+        console.log(doc.id, '=>', doc.data());
+         });
     })
     .catch((err) => {
       console.log('Error getting documents', err);
