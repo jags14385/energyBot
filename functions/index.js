8f8d1bfcd94d8cc247b1540b46a735b2e08e4b84
Jags <jagannath.vaikuntham@originenergy.com.au>
2018-07-05 14:54:03 +1000

    log request

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
    
-console.log("REQUEST :: ", request);
+console.log("REQUEST :: ", JSON.stringify(request.body));
     
 response.setHeader('Content-Type', 'application/json');
 response.send(JSON.stringify({"fulfillmentText": "Hai Chai Hooli" }));
