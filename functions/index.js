ef1982f0581f02d4c6b7c050b5dbab1cee544ee4
chai hooli <chai.hooli@originenergy.com.au>
2018-07-05 15:29:37 +1000

    switch added for different requests

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,23 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
    
 console.log("REQUEST :: ", JSON.stringify(request.body));
+
+switch (request){
+    case '100kwh':
+            response.setHeader('Content-Type', 'application/json');
+            response.send(JSON.stringify({"fulfillmentText": "Response for 100kwh" }));
+            break;
+
+    case '600kwh':
+            response.setHeader('Content-Type', 'application/json');
+            response.send(JSON.stringify({"fulfillmentText": "Response for 600kwh" }));
+            break;
+
+     default:
+            response.setHeader('Content-Type', 'application/json');
+            response.send(JSON.stringify({"fulfillmentText": "Default Response" }));
+}
     
-response.setHeader('Content-Type', 'application/json');
-response.send(JSON.stringify({"fulfillmentText": "Hai Chai Hooli" }));
+//response.setHeader('Content-Type', 'application/json');
+//response.send(JSON.stringify({"fulfillmentText": "Hai Chai Hooli" }));
  });
