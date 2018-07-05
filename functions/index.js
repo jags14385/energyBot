6fd2ef49b7c481337df5f414a31c926d634f03ec
chai hooli <chai.hooli@originenergy.com.au>
2018-07-05 14:42:16 +1000

    v2 api response

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,9 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
  exports.helloWorld = functions.https.onRequest((request, response) => {
 
 response.setHeader('Content-Type', 'application/json');
-response.send(JSON.stringify({ "speech": "Hai Chai Hooli", "displayText": "Hai Chai Hooli" })); 
+response.send(JSON.stringify({"fulfillmentText": "Hai Chai Hooli" }));
  });
