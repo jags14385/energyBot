11b024bced7dad25bc3cb02d4d5f73e4b79f2f71
Jags <jagannath.vaikuntham@originenergy.com.au>
2018-07-06 12:16:44 +1000

    This fucking works

--- functions/index.js
+++ functions/index.js
@@ -6,39 +6,106 @@ admin.initializeApp(functions.config().firebase);
 var db = admin.firestore();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-   
-console.log("REQUEST :: ", JSON.stringify(request.body));
-var responseText , avgCost;
 
-var energyConsumption = request.body.queryResult.parameters['annual-energy-consumption'];
+    console.log("REQUEST :: ", JSON.stringify(request.body));
+    var responseText, avgCost;
 
-if (energyConsumption !== null ) {
-    avgCost = energyConsumption / 4 ;
-    responseText = "Avg Cost for appliance per year is : " + avgCost;
-}
+    var energyConsumption = request.body.queryResult.parameters['annual-energy-consumption'];
 
-var username = request.body.queryResult.parameters['username'][0].toLocaleLowerCase();
-var device = request.body.queryResult.parameters['device'][0];
+    if (energyConsumption !== null) {
+        avgCost = energyConsumption / 4;
+        responseText = "Avg Cost for appliance per year is : " + avgCost;
+    }
 
-if ( username !== null && device !== null) {
-    console.log("INFO :: ", username , device );
-}
+    var username = request.body.queryResult.parameters['username'][0].toLocaleLowerCase();
+    var device = request.body.queryResult.parameters['device'][0].toLocaleLowerCase();
 
-var query = db.collection('users').where('username','==',`${username}`);
+    if (username !== null && device !== null) {
+        console.log("INFO :: ", username, device);
+    }
 
-query.get().then(snapshot => {
-    snapshot.forEach( doc => {
-        if(doc.exists) {
-          console.log(doc.id, '=>', doc.data());
-        } else {
-          console.log('no doc exits');
-        }
+    var query = db.collection('users').where('username', '==', `${username}`);
+    var consumption,consumption1,consumption2;
+    var dishWasherSpec, refrigatorSpec;
+
+    var p1 = query.get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            if (doc.exists) {
+                console.log(doc.id, '=>', doc.data());
+                console.log("DEVICE :: ", `${device}`);
+
+                dishWasherSpec = doc.data()['dishwasher'];
+                refrigatorSpec = doc.data()['refrigator'];
+
+                console.log(dishWasherSpec.split('-'));
+                console.log(refrigatorSpec.split('-'));
+            } else {
+                console.log('no doc exits');
+            }
+        })
     })
-  }).catch((err) => {
-    console.log('Error getting documents', err);
-  });
+    .then(() => {
+        return db.collection('fridge').get().then(snap => {
+            snap.forEach(docu => {
+                console.log(docu.id, '=====>', docu.data());
+                var fridgeModel = refrigatorSpec.split('-')[1];
+                console.log(fridgeModel);
+                console.log(docu.data()[fridgeModel]);
+                consumption1 = docu.data()[fridgeModel];
+                console.log("fridge Consumption ::", consumption1)
+            });
+        });
+    })
+    .then(() => {
+        return db.collection('washer').get().then(snap => {
+            snap.forEach(docu => {
+                console.log(docu.id, '=====>', docu.data());
+                var dishWasherModel = dishWasherSpec.split('-')[1];
+                console.log(dishWasherModel);
+                console.log(docu.data()[dishWasherModel]);
+                consumption2 = docu.data()[dishWasherModel];
+                console.log("washer Consumption ::", consumption2)
+            });
+        });
+    })
+    .then(() => {
+        consumption = consumption1 + consumption2 ;
+        responseText = "For User: " +username + " The annual energy consumption is : " + consumption;
+        console.log(responseText);
+        response.setHeader('Content-Type', 'application/json');
+        response.send(JSON.stringify({
+            fulfillmentText: responseText
+        })
+    );    
+});
+});
 
-  responseText = username + device ;
-  response.setHeader('Content-Type', 'application/json');
-  response.send(JSON.stringify({fulfillmentText:  responseText}));
- });
+    // var p2 = p1.then(() => {
+    //     db.collection('fridge').get().then(snap => {
+    //         snap.forEach(docu => {
+    //             console.log(docu.id, '=====>', docu.data());
+    //             var fridgeModel = refrigatorSpec.split('-')[1];
+    //             console.log(fridgeModel);
+    //             console.log(docu.data()[fridgeModel]);
+    //             consumption1 = docu.data()[fridgeModel];
+    //             console.log("fridge Consumption ::", consumption1)
+    //         });
+    //     });
+    // });
+
+    // var p3 = p1.then(() => {
+    //     db.collection('washer').get().then(snap => {
+    //         snap.forEach(docu => {
+    //             console.log(docu.id, '=====>', docu.data());
+    //             var dishWasherModel = dishWasherSpec.split('-')[1];
+    //             console.log(dishWasherModel);
+    //             console.log(docu.data()[dishWasherModel]);
+    //             consumption2 = docu.data()[dishWasherModel];
+    //             console.log("washer Consumption ::", consumption2)
+    //         });
+    //     });
+    // });
+
+    // Promise.all( [ p2 , p3 ] ).
+    // });
